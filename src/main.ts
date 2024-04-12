7e8e64f83bfa50de7464d3e0a90c42cfd58d236b
erkinov97 <erkinov997@gmail.com>
2024-04-12 23:09:00 +0500

    feat added .env and pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia'
 
 // Import the global styles
 import 'element-plus/dist/index.css'
@@ -7,16 +8,20 @@ import "./style.css";
 // Import the App component
 import App from "./App.vue";
 
+
 // Import the plugins
 import ElementPlus from 'element-plus'
 import router from "./router";
 
 // Create the app instance
 const app = createApp(App);
+const pinia = createPinia()
+
 
 // register the components
 app.use(ElementPlus)
 app.use(router);
+app.use(pinia)
 
 
 // Mount the app

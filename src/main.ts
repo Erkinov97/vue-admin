3cabfee1c98c6db2c03902f96db627c45f060572
erkinov97 <erkinov997@gmail.com>
2024-04-12 22:26:04 +0500

    feat added element plus

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { createApp } from "vue";
 
 // Import the global styles
+import 'element-plus/dist/index.css'
 import "./style.css";
 
 // Import the App component
 import App from "./App.vue";
 
-// Import the router
+// Import the plugins
+import ElementPlus from 'element-plus'
 import router from "./router";
 
 // Create the app instance
 const app = createApp(App);
 
 // register the components
+app.use(ElementPlus)
 app.use(router);
+
 
 // Mount the app
 app.mount("#app");
